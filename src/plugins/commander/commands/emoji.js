4c7842bb8af6d446a6f98bff34a456c2266d5815
Naughty <dorminsama@gmail.com>
2021-07-01 19:06:48 -0300

    +!emoji

---
+++ src/plugins/commander/commands/emoji.js
@@ -0,0 +1,50 @@
+const { MessageEmbed } = require('discord.js');
+const Command = require('../structs/Command.js');
+const FormatterPlugin = require('../../fmt');
+
+const EMOJI_REGEX = /<a?:(\w+):(\d+)>/;
+
+class EmojiCommand extends Command {
+    static get deps() {
+        return [
+            FormatterPlugin
+        ];
+    }
+
+    constructor(bot) {
+        super(bot);
+        this.aliases = ['emoji', 'emote', 'jumbo'];
+
+        this.shortdesc = 'Enlarges an emoji';
+        this.desc = `
+            Posts an embed that enlarges an emote in the message or reference.`;
+        this.usages = [
+            '!emoji :custom_emote:',
+            '!emote <reply to a message with an emote>'
+        ];
+    }
+
+    async call(message, content) {
+        let match = content.match(EMOJI_REGEX);
+
+        if (match === null && message.reference) {
+            const referenced = await message.channel.messages.fetch(message.reference.messageID);
+
+            match = referenced.content.match(EMOJI_REGEX);
+        }
+
+        if (match === null) {
+            await message.channel.send('No emoji found in your message');
+            return;
+        }
+
+        await message.channel.send(
+            new MessageEmbed()
+                .setTitle(`Emoji link: ${match[1]}`)
+                .setURL(`https://cdn.discordapp.com/emojis/${match[2]}.png`)
+                .setImage(`https://cdn.discordapp.com/emojis/${match[2]}.png`)
+        );
+    }
+}
+
+module.exports = EmojiCommand;
