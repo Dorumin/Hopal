f488c322272c02ef0c3593f8487f67f46e50d957
Dorumin <dorminsama@gmail.com>
2022-07-01 13:07:32 -0300

    Create emojis

--- src/plugins/commander/commands/emoji.js
+++ src/plugins/commander/commands/emoji.js
@@ -3,6 +3,7 @@ const Command = require('../structs/Command.js');
 const FormatterPlugin = require('../../fmt');
 
 const EMOJI_REGEX = /<(a?):(\w+):(\d+)>/;
+const EMOJI_REGEX_G = /<(a?):(\w+):(\d+)>/g;
 
 class EmojiCommand extends Command {
     static get deps() {
@@ -25,6 +26,10 @@ class EmojiCommand extends Command {
     }
 
     async call(message, content) {
+        if (content.startsWith('new ')) {
+            return this.createEmojis(message, content);
+        }
+
         let match = content.match(EMOJI_REGEX);
 
         if (match === null && message.reference) {
@@ -53,6 +58,49 @@ class EmojiCommand extends Command {
                     .setImage(`https://cdn.discordapp.com/emojis/${id}.${ext}`)
             ]
         });
+    }
+
+    getEmojiUrl(emoji) {
+        const ext = emoji[1] === 'a' ? 'gif' : 'png';
+        const id = emoji[3];
+
+        return `https://cdn.discordapp.com/emojis/${id}.${ext}?size=256`;
+    }
+
+    async createEmojis(message, content) {
+        const copies = Array.from(content.matchAll(EMOJI_REGEX_G));
+        const emotes = [];
+
+        if (copies.length) {
+            for (const copy of copies) {
+                const url = this.getEmojiUrl(copy);
+
+                emotes.push({
+                    url,
+                    name: copy[2]
+                });
+            }
+        } else {
+            if (message.attachments.size) {
+                for (const attachment of message.attachments.values()) {
+                    emotes.push({
+                        url: attachment.url,
+                        name: content.slice(4).trim()
+                    });
+                }
+            }
+        }
+
+        if (!emotes.length) {
+            await message.channel.send('no emotes');
+            return;
+        }
+
+        for (const emote of emotes) {
+            await message.guild.emojis.create(emote.url, emote.name);
+        }
+
+        await message.channel.send(`Created ${emotes.length} emotes`);
     }
 }
 
